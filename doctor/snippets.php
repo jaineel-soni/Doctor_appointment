c1181ff608e36ee871571e77688b6491338ed198
jaineel-soni <jaineelsoni924@gmail.com>
2023-08-05 01:10:20 +0530

    Initial Commit

---
+++ doctor/snippets.php
@@ -0,0 +1,197 @@
+<!doctype html>
+                        <html>
+                            <head>
+                                <meta charset='utf-8'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <title>Snippet - BBBootstrap</title>
+                                <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' rel='stylesheet'>
+                                <link href='#' rel='stylesheet'>
+                                <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+                                <style>::-webkit-scrollbar {
+                                  width: 12px;
+                                }
+                                /* Track */
+                                ::-webkit-scrollbar-track {
+                                  background: #f1f1f1; 
+                                }
+                                 
+                                /* Handle */
+                                ::-webkit-scrollbar-thumb {
+                                  background: #888; 
+                                }
+                                
+                                /* Handle on hover */
+                                ::-webkit-scrollbar-thumb:hover {
+                                  background: #555; 
+                                } body {
+    background-color: #673AB7
+}
+
+.main {
+    background-color:#673AB7;
+    padding-top: 100px;
+    padding-bottom: 50px;
+    padding-left: 130px;
+}
+
+.card {
+    background-color: #fff;
+    box-shadow: 0 8px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    width: 300px;
+    border-radius: 10px
+}
+
+.pay {
+    font-size: 18px;
+    color: #000;
+    font-weight: 600;
+    margin-top: 7px;
+}
+
+.amount {
+    background-color: #f3f3fd;
+    border-radius: 8px;
+}
+
+.inner {
+    padding-top: 8px;
+    padding-bottom: 8px;
+    padding-left: 19px;
+    padding-right: 19px;
+}
+
+.dollar {
+    color: #a49ecc;
+    font-size: 20px;
+    font-weight: 500;
+}
+
+.total {
+    font-size: 20px;
+    font-weight: 500;
+    color: #6458ab
+}
+
+.labeltxt {
+    font-size: 13px;
+    color: #adb5bd;
+    font-weight: 500;
+    margin-top: 5px;
+}
+
+.image {
+    position: relative;
+    top: 39px;
+    right: 13px
+}
+
+.form-control {
+    border: 2px solid #c4c2d0;
+    border-radius: 7px;
+}
+
+.inputtxt {
+    color: #000 !important;
+    font-weight: bold;
+    font-size: 14px;
+    padding-top: 21px;
+    padding-bottom: 21px;
+}
+
+.exptxt {
+    color: #adb5bd;
+    font-size: 13px;
+    font-weight: 500;
+}
+
+.cvvtxt {
+    color: #adb5bd;
+    font-size: 13px;
+    font-weight: 500;
+}
+
+.expiry {
+    padding-top: 21px;
+    padding-bottom: 21px;
+    width: 120px;
+    font-size: 14px;
+    font-weight: bold;
+}
+
+.cvv {
+    padding-top: 21px;
+    padding-bottom: 21px;
+    width: 120px;
+}
+
+.cancel {
+    background-color: #fff;
+    color: #adb5bd;
+    font-size: 13px;
+    font-weight: 500;
+    border: none;
+    position: relative;
+    top: 24px;
+}
+
+.cancel:hover {
+    background-color: #fff;
+    color: #adb5bd;
+    font-size: 13px;
+    font-weight: 500;
+    border: none;
+}
+
+.cancel:focus {
+    background-color: #fff;
+    color: #adb5bd;
+    font-size: 13px;
+    font-weight: 500;
+    border: 1px solid #adb5bd;
+}
+
+.payment {
+    font-weight: 600;
+    font-size: 14px;
+    background-color: #673AB7;
+    border: none;
+    width: 150px;
+    padding-top: 10px;
+    padding-bottom: 21px;
+    padding-left: 29px;
+    padding-right: 10px;
+    border-radius: 11px;
+}
+
+.payment:hover {
+    font-weight: 600;
+    font-size: 14px;
+    background-color: #673AB7;
+    border: none;
+    width: 150px;
+    padding-top: 15px;
+    padding-bottom: 21px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 11px;
+}
+
+.payment:focus {
+    background-color: #673AB7;
+}</style>
+                                </head>
+                                <body className='snippet-body'>
+                                <div class="container mt-4 d-flex justify-content-center main">	<div class="card">	<div class="d-flex justify-content-between px-3 pt-4">	<span class="pay">Pay amount</span>	<div class="amount"><div class="inner"><span class="dollar">$</span><span class="total">32</span></div></div>	</div>	<div class="px-3 pt-3">	<label for="card number" class="d-flex justify-content-between"><span class="labeltxt">CARD NUMBER</span><img src="https://img.icons8.com/color/48/000000/mastercard-logo.png" width="25" class="image" /></label>	<input type="number" name="number" class="form-control inputtxt" placeholder="8881 2545 2545 2245">	</div>	<div class="d-flex justify-content-between px-3 pt-4">	<div><label for="date" class="exptxt"> Expiry </label><input type="number" name="number" class="form-control expiry" placeholder="MM / YY"></div>	<div><label for="cvv" class="cvvtxt">CVV / CVC</label><input type="number" name="number" class="form-control cvv"></div>	</div>	<div class="d-flex justify-content-between px-3 pt-4 pb-4">	<div><button type="button" class="btn btn-light cancel">Cancel</button></div>	<div><button type="button" class="btn btn-primary payment">Make Payment</button></div>	</div>	</div>
+</div>
+                                <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js'></script>
+                                <script type='text/javascript' src='#'></script>
+                                <script type='text/javascript' src='#'></script>
+                                <script type='text/javascript' src='#'></script>
+                                <script type='text/javascript' src="#"></script>
+                                <script type='text/javascript'>var myLink = document.querySelector('a[href="#"]');
+                                myLink.addEventListener('click', function(e) {
+                                  e.preventDefault();
+                                });</script>
+                            
+                                </body>
+                            </html>
